29194debaa855daeb77946a5f1eabd51a97e087a
bikingeskimo <roger.schei.bergheim@live.no>
2016-09-14 12:47:07 +0200

    Fiks

---
+++ 3D/Testkube/build/CMakeFiles/Testkube.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Testkube.dir/main.cpp.o"
+  "Testkube.pdb"
+  "Testkube"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Testkube.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
